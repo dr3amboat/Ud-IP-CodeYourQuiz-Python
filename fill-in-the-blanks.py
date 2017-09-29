f4f4b904ddcde32121f05b3c8736c33d23e572b0
bcko <gobcko@gmail.com>
2017-09-29 06:15:09 -0400

    working

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -1,49 +1,41 @@
-# IPND Stage 2 Final Project
+def play_game(difficulty):
+    print "You've chosen %s!" %difficulty
+    
+    remaining_guesses = 5
+    print "You will get %s guesses per problem" %remaining_guesses
 
-# You've built a Mad-Libs game with some help from Sean.
-# Now you'll work on your own game to practice your skills and demonstrate what you've learned.
+    ANSWER_LIST = ['one', 'two', 'three', 'four']
+    user_answer = ['__1__', '__2__', '__3__', '__4__']
 
-# For this project, you'll be building a Fill-in-the-Blanks quiz.
-# Your quiz will prompt a user with a paragraph containing several blanks.
-# The user should then be asked to fill in each blank appropriately to complete the paragraph.
-# This can be used as a study tool to help you remember important vocabulary!
+    current_problem_number = 1
 
-# Note: Your game will have to accept user input so, like the Mad Libs generator,
-# you won't be able to run it using Sublime's `Build` feature.
-# Instead you'll need to run the program in Terminal or IDLE.
-# Refer to Work Session 5 if you need a refresher on how to do this.
+    while remaining_guesses > 0 and current_problem_number <=4:
+        print "The current paragraph reads as such:\n Let's count! zero, %s, %s, %s, %s, five, six..." %(user_answer[0], user_answer[1], user_answer[2], user_answer[3])
+        user_problem_answer = raw_input("What should be substituted in for %s?\n" %user_answer[current_problem_number-1])
+        if user_problem_answer == ANSWER_LIST[current_problem_number-1]:
+            print "Correct!"
+            user_answer[current_problem_number-1] = user_problem_answer
+            current_problem_number += 1
+        else :
+            remaining_guesses -= 1
+            print "That isn't the correct answer!  Let's try again; you have %s try(s) left!" % remaining_guesses
+    
+    
+    if current_problem_number == len(ANSWER_LIST) + 1:
+        print "You won!"
+    else : 
+        print "You've failed too many straight guesses!  Game over!"
+        
 
-# To help you get started, we've provided a sample paragraph that you can use when testing
-# your code.
-# Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
+def main():
+    while True:
+        ChosenDifficulty = raw_input('Select a game difficulty : easy, medium, and hard.\n')
+        if ChosenDifficulty in ['easy', 'medium', 'hard']:
+            play_game(ChosenDifficulty)
+            break
+        else:
+            print "That's not an option!"
 
-SAMPLE = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
-adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
-don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
-tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
 
-# The answer for ___1___ is 'function'. Can you figure out the others?
-
-# We've also given you a file called fill-in-the-blanks.pyc which is a working version of the project.
-# A .pyc file is a Python file that has been translated into "byte code".
-# This means the code will run the same as the original .py file, but when you open it
-# it won't look like Python code! But you can run it just like a regular Python file
-# to see how your code should behave.
-
-# Hint: It might help to think about how this project relates to the Mad Libs generator you built with Sean.
-# In the Mad Libs generator, you take a paragraph and replace all instances of NOUN and VERB.
-# How can you adapt that design to work with numbered blanks?
-
-# If you need help, you can sign up for a 1 on 1 coaching appointment: https://calendly.com/ipnd-1-1/20min/
-
-DIFFICULTY_LIST = ['easy', 'medium', 'hard']
-
-DIFFICULTY_PROMPT = 'Please select a game difficulty by typing it in!\n\
-Possible choices include easy, medium, and hard.\n'
-
-DIFFICULTY_INPUT = raw_input(DIFFICULTY_PROMPT)
-
-if DIFFICULTY_INPUT in DIFFICULTY_LIST:
-    print "You've chosen %s!" % DIFFICULTY_INPUT
-else:
-    print "That's not an option!"
+if __name__ == "__main__":
+    main()
